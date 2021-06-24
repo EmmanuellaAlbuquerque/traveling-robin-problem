3a834769b5ee62b5812a7d124dac4df9dbc1b8a8
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2021-06-24 16:21:26 -0300

    feat: adding vnd structure

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from robin_file_reader import RobinFileReader
 from trp import TRP
-from vnd import swap
+from vnd import VND
+from copy import deepcopy
 
 # file_path = Path("../instances/n5.txt").absolute()
 # file_path = Path("../instances/n10p4.txt").absolute()
@@ -14,30 +15,41 @@ file_path = Path("../instances/n52p11.txt").absolute()
 # file_path = Path("../instances_apa_cup/cup1.txt").absolute()
 # file_path = Path("../instances_apa_cup/cup2.txt").absolute()
 # file_path = Path("../instances_apa_cup/cup3.txt").absolute()
+
+
+def showSolution(solution):
+    for route in solution:
+        print(', '.join(map(str, route)), end=';\n')
+
+
+def f(agent_list):
+    oF = 0
+    for i in agent_list:
+        oF += i['cost']
+    return oF
+
+
 file = open(file_path, 'r')
 
 (dimension, p, cost_matrix) = RobinFileReader(file).getResult()
 
+# constructive algorithm solution
+print('constructive algorithm solution')
 trp = TRP(dimension, p, cost_matrix)
+trp_solution = trp.run()
+(initial_cost, trp_agent_list) = trp.calculateTotalCost()
+# print(agent_list)
+showSolution(trp_solution)
+print('\n')
+
 print('--- changes ---')
-initial_solution = trp.run()
+# variable neighbourhood descent solution
+vnd = VND()
+(vnd_solution, vnd_agent_list) = vnd.run(
+    deepcopy(trp_solution), deepcopy(trp_agent_list), cost_matrix)
+# print(vnd_agent_list)
+print('variable neighbourhood descent solution')
+showSolution(vnd_solution)
 
-(initial_total_cost, agent_list) = trp.calculateTotalCost()
-# print('total cost:', initial_total_cost)
-# print(agent_list)
-# print(initial_solution)
-(initial_solution, agent_list) = swap(
-    initial_solution, agent_list, cost_matrix)
-
-new_total = 0
-for i in agent_list:
-    new_total += i['cost']
-
-print('total cost:', initial_total_cost)
-print('new total cost:', new_total)
-# print(agent_list)
-
-# --------------------------------------------
-
-# for element in initial_solution:
-#     print(', '.join(map(str, element)), end=';\n')
+print('total cost:', f(trp_agent_list))
+print('new total cost:', f(vnd_agent_list))
--- src/vnd.py
+++ src/vnd.py
@@ -1,66 +1,93 @@
-def swap(initial_solution, agent_list, cost_matrix):
-    best_value = float('inf')
-    best_i = 0
-    best_j = 0
-    best_s = 0
-    for s_id in range(0, len(initial_solution)):
-        s = initial_solution[s_id][:]
-        oF = agent_list[s_id]['cost']
+class VND:
+    def __init__(self):
+        self.switch = {
+            1: self.swap
+        }
 
-        # for with improved constant factors
-        for i in range(1, len(s) - 1):
-            for j in range(i+1, len(s) - 1):
-                # simulating the swap
-                swapped_element1 = s[i]
-                swapped_element2 = s[j]
-                swapped_element1_left = s[i-1]
-                swapped_element1_right = s[i+1]
-                swapped_element2_left = s[j-1]
-                swapped_element2_right = s[j+1]
+    def run(self, initial_solution, agent_list, cost_matrix):
+        r = len(self.switch)
+        k = 1  # tipo de estrutura de vizinhança corrente
+        while (k <= r):
+            func = self.switch.get(k)
+            (sLine, new_agent_list) = func(
+                initial_solution, agent_list, cost_matrix)
+            if (self.f(new_agent_list) < self.f(agent_list)):
+                initial_solution = sLine
+                agent_list = new_agent_list
+                k = 1
+            else:
+                k = k + 1
+        return (initial_solution, agent_list)
 
-                # particular situation of adjacent vertices
-                if (j == i+1):
-                    oFLine = oF - \
-                        cost_matrix[swapped_element1_left][swapped_element1] - \
-                        cost_matrix[swapped_element2][swapped_element2_right] + \
-                        cost_matrix[swapped_element1_left][swapped_element2] + \
-                        cost_matrix[swapped_element1][swapped_element2_right]
-                    # print('f0',
-                    #       '- c(', swapped_element1_left, swapped_element1,
-                    #       ') - c(', swapped_element2, swapped_element2_right,
-                    #       ') + c(', swapped_element1_left, swapped_element2,
-                    #       ') + c(', swapped_element1, swapped_element2_right, ')')
-                else:
-                    oFLine = oF - \
-                        cost_matrix[swapped_element1_left][swapped_element1] - \
-                        cost_matrix[swapped_element2][swapped_element2_right] + \
-                        cost_matrix[swapped_element1_left][swapped_element2] + \
-                        cost_matrix[swapped_element1][swapped_element2_right]
+    def f(self, agent_list):
+        oF = 0
+        for i in agent_list:
+            oF += i['cost']
+        return oF
 
-                    # print('f0',
-                    #       '- c(', swapped_element1_left, swapped_element1,
-                    #       ') - c(', swapped_element2, swapped_element2_right,
-                    #       ') + c(', swapped_element1_left, swapped_element2,
-                    #       ') + c(', swapped_element1, swapped_element2_right, ')')
-                if (oFLine < best_value):
-                    best_value = oFLine
-                    best_i = i
-                    best_j = j
-                    best_s = s_id
-                # print('ObjectFunction(result):', oFLine)
-        # print(
-        #     '---------------------------------------------------------------------')
-    if best_value < agent_list[best_s]['cost']:
-        print('agent', best_s+1)
-        print(initial_solution[best_s],
-              'cost<initial>:', agent_list[best_s]['cost'])
-        agent_list[best_s]['cost'] = best_value
-        aux = initial_solution[best_s][best_i]
-        initial_solution[best_s][best_i] = initial_solution[best_s][best_j]
-        initial_solution[best_s][best_j] = aux
-        print(initial_solution[best_s], 'cost<swap>:',
-              agent_list[best_s]['cost'], 'best(i, j):', best_i, best_j, '\n')
-    else:
-        print('cost<swap>: notbetter, agent', best_s+1,
-              '<compared>: ', best_value, '<', agent_list[best_s]['cost'])
-    return (initial_solution, agent_list)
+    def swap(self, initial_solution, agent_list, cost_matrix):
+        best_value = float('inf')
+        best_i = 0
+        best_j = 0
+        best_s = 0
+        for s_id in range(0, len(initial_solution)):
+            s = initial_solution[s_id][:]
+            oF = agent_list[s_id]['cost']
+
+            # for with improved constant factors
+            for i in range(1, len(s) - 1):
+                for j in range(i+1, len(s) - 1):
+                    # simulating the swap
+                    swapped_element1 = s[i]
+                    swapped_element2 = s[j]
+                    swapped_element1_left = s[i-1]
+                    swapped_element1_right = s[i+1]
+                    swapped_element2_left = s[j-1]
+                    swapped_element2_right = s[j+1]
+
+                    # particular situation of adjacent vertices
+                    if (j == i+1):
+                        oFLine = oF - \
+                            cost_matrix[swapped_element1_left][swapped_element1] - \
+                            cost_matrix[swapped_element2][swapped_element2_right] + \
+                            cost_matrix[swapped_element1_left][swapped_element2] + \
+                            cost_matrix[swapped_element1][swapped_element2_right]
+                        # print('f0',
+                        #       '- c(', swapped_element1_left, swapped_element1,
+                        #       ') - c(', swapped_element2, swapped_element2_right,
+                        #       ') + c(', swapped_element1_left, swapped_element2,
+                        #       ') + c(', swapped_element1, swapped_element2_right, ')')
+                    else:
+                        oFLine = oF - \
+                            cost_matrix[swapped_element1_left][swapped_element1] - \
+                            cost_matrix[swapped_element2][swapped_element2_right] + \
+                            cost_matrix[swapped_element1_left][swapped_element2] + \
+                            cost_matrix[swapped_element1][swapped_element2_right]
+
+                        # print('f0',
+                        #       '- c(', swapped_element1_left, swapped_element1,
+                        #       ') - c(', swapped_element2, swapped_element2_right,
+                        #       ') + c(', swapped_element1_left, swapped_element2,
+                        #       ') + c(', swapped_element1, swapped_element2_right, ')')
+                    if (oFLine < best_value):
+                        best_value = oFLine
+                        best_i = i
+                        best_j = j
+                        best_s = s_id
+                    # print('ObjectFunction(result):', oFLine)
+            # print(
+            #     '---------------------------------------------------------------------')
+        if best_value < agent_list[best_s]['cost']:
+            print('agent', best_s+1)
+            print(initial_solution[best_s],
+                  'cost<initial>:', agent_list[best_s]['cost'])
+            agent_list[best_s]['cost'] = best_value
+            aux = initial_solution[best_s][best_i]
+            initial_solution[best_s][best_i] = initial_solution[best_s][best_j]
+            initial_solution[best_s][best_j] = aux
+            print(initial_solution[best_s], 'cost<swap>:',
+                  agent_list[best_s]['cost'], 'best(i, j):', best_i, best_j, '\n')
+        else:
+            print('cost<swap>: notbetter, agent', best_s+1,
+                  '<compared>: ', best_value, '<', agent_list[best_s]['cost'])
+        return (initial_solution, agent_list)
